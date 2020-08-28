5154b739faaa05aad19ed420c8a6e68b2dfd7ce9
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2020-08-27 21:32:22 -0500

    Add files via upload

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -15,16 +15,41 @@ public class Taller5 {
      * @return la suma de todos los numeros sumados.
      */
     public static int suma (int[]array){
-        int acum = 0; // T2(n)=c1
-        for (int i = 0; i < array.length; i++) // T3(n)=c2+c3*n
-            acum = acum + array[i]; // T4(n)=c4*n
-        return acum; // T5(n)=c5
-        /* T(n)= c1 + c2+c3*n + c4*n + c5
-         * Suma --> O(c4*n)
+        long t= System.currentTimeMillis();//T2(n)=c1
+        int acum = 0; // T3(n)=c2
+        
+        for (int i = 0; i < array.length; i++){ // T4(n)=c3+c4*n
+        acum = acum + array[i]; // T5(n)=c5*n
+    }
+        long ti= System.currentTimeMillis(); //T6(n)= c6*n
+         System.out.print(t-ti); //T7(n)=c7
+        return acum; // T8(n)=c8
+        /* T(n)= c1 + c2+c3+c4*n + c5*n+ c6*n+ c7+ c8
+         * Suma --> O(c6*n)
          * Producto --> T(n) es O(n)
          */
     }
-
+    public static int sumaRec(int[] array, int pos){
+     int tam= pos; //T2(n)=c1
+     int res; //T3(n)=c2
+     if(tam>=array.length) { //T4(n)=c3+c4*n
+         return 0; // T5(n)=c5*n
+    }
+    return res = array[tam]+ sumaRec(array, tam+1); //T6(n)=c6+T(n-1) --> T6(n)=c6+c7*n
+     /* T(n)=c1+ c2+ c3+c4*n + c5*n+ c6+c7*n
+      * Suma --> O(c7*n)
+      * Producto --> O(n)
+      * T(n) es O(n)
+      */
+    }
+    public static void medirsumrec(int[] a){
+      long ti = System.currentTimeMillis();
+      sumaRec(a,0); //--> O(n)
+      long tf = System.currentTimeMillis();
+      System.out.println(tf);
+      System.out.println(ti);
+      System.out.println(tf-ti);
+    }
     /**
      * @param num es el numero el cual se utiliza para ser multiplicado.
      * El método mul tiene la intención de hacer la multiplicación
