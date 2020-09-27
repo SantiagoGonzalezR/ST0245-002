75db0492d9419f9b9bbcf242f1448e509015307f
mvasqueze <68928428+mvasqueze@users.noreply.github.com>
2020-09-27 00:56:00 -0500

    Add files via upload

--- laboratorios/lab03/ejercicioEnLinea/TecladoRoto.java
+++ laboratorios/lab03/ejercicioEnLinea/TecladoRoto.java
@@ -1,32 +1,31 @@
 import java.util.*;
-public class TecladoRoto{
+public class TecladoRoto2{
     //Punto 2.1
     public static void teclado(String a){
         LinkedList<Character>texto = new LinkedList<>();
+        int j=0;
+        boolean inc=false;
+        boolean fin=true;
         for (int i=0; i<a.length(); i++){
-            switch (a.charAt(i)) {
-                case '[':
-                    int j=0;
-                    while (a.charAt(i)!=']'){
-                        texto.add(j, a.charAt(i));
-                        if (a.charAt(i+1)=='[' && i+1<a.length()) break;
-                        i++;
-                        j++;
-                    } 
-                    texto.removeFirstOccurrence('[');
-                    break;
-                case ']':
-                    while (a.charAt(i)!='['){
-                        texto.addLast(a.charAt(i));
-                        break;
-                    } texto.removeFirstOccurrence(']');
-                     
-                    break;
-                default:
-                    texto.add(a.charAt(i));
-                    break;
-            }
+        if(a.charAt(i)=='['){
+            inc=true;
+            fin=false;
+            j=0;
+        }
+        if(fin){
+            texto.add(a.charAt(i));
+        }if(a.charAt(i)==']'){
+            inc=false;
+            fin=true;
+        }
+        if(inc){
+            texto.add(j++,a.charAt(i));
+        }
+        }
+        while(texto.contains('[') || texto.contains(']')){
+            texto.removeFirstOccurrence('[');
+            texto.removeFirstOccurrence(']');
         }
         System.out.println(texto);
-    }
-}
+       }
+ }           
